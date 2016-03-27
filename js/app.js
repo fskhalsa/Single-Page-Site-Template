9406e5e8226a968dde32f079a428aacbbf746a99
Fateh Khalsa <fskhalsa@gmail.com>
2016-03-27 03:55:33 -0600

    Update page change code to operate based on percentage of viewport covered, vs percentage of page on screen. Remove scroll to page on resize code. Update code for responsive design. Add media query helper functions.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,13 @@ var PAGESCROLL_TIMEOUT_ID;
 var PAGESCROLL_ANIMATION_SPEED = 850;
 
 /* Plugins */
-$.getScript('/js/jquery.isOnScreen.min.js');
+(function ($) {
+    $.fn.coversXPercentageOfViewport = function(x){
+		var offsetFromViewport = Math.abs($(this).offset().top - $(window).scrollTop());
+		var minimumOffset = $(window).height() * Math.abs(x - 1.0);
+		return (offsetFromViewport <= minimumOffset);
+    };
+})(jQuery);
 
 /* Page-Template Code */
 // On page load:
@@ -19,36 +25,49 @@ _setPageHeights();
 
 $(window).resize(function(event) {
 	_setPageHeights();
-	_scrollToPage($('.page.current-page'));
 });
 
 function _setPageHeights() {
 	var viewportHeight = $(window).height();
-	$('.page').height(viewportHeight);
+	if (_screenSizeIsMDAndUp()) {
+		$('.page').height(viewportHeight);
+	}
+	else {
+		$('.page').css('height', 'auto');
+		$('.page').each(function(i, page) {
+			var currentPageHeight = $(page).height();
+			var newPageHeight = Math.max(viewportHeight, currentPageHeight);
+			$(page).height(newPageHeight);
+		});
+	}
 }
 
 // Handle window scrolling
-$.getScript('/js/jquery.isOnScreen.min.js', function() {
-	$(window).scroll(function(event) {
-		// Scroll page into full view
-		$('.page').each(function(i, page) {
-			if ($(page).isOnScreen(1.0, 0.5) && !$(page).hasClass('current-page')) {
+$(window).scroll(function(event) {
+	$('.page').each(function(i, page) {
+		if ($(page).coversXPercentageOfViewport(0.5) && !$(page).hasClass('current-page')) {
+			if (_screenSizeIsMDAndUp()) {
+				// Scroll page into full view, and set current page (animate backgrounds, etc)
 				clearTimeout(PAGESCROLL_TIMEOUT_ID);
 				PAGESCROLL_TIMEOUT_ID = setTimeout(_scrollToPage.bind(null, $(page)), 100);
-				return false;
 			}
-		});
-		// Set selected nav button on scroll
-		var $currentPage = $('.page.current-page');
-		$('.navbar li').removeClass('selected');
-		$('.navbar li[data-nav-page='+$currentPage.attr('id')+']').addClass('selected');
-		// Fade background with scroll
-		// var pageVisibilityPercentage = ($('.page.current-page').offset().top - $(window).scrollTop()) / $(window).height();
-		// var currentPageVisibilityPercentage = Math.abs((pageVisibilityPercentage - 0.5) * 2);
-		// var currentPageOffsetPercentage = Math.abs(currentPageVisibilityPercentage - 1);
-		// var opacity = ((currentPageOffsetPercentage * 0.2) + 0.2).toFixed(2);
-		// $('.page').css('background-color', 'rgba(0, 0, 0, '+opacity+')');
+			else {
+				// Set current page (animate backgrounds, etc)
+				_setCurrentPage($(page));
+			}
+			return false;
+		}
 	});
+	// Set selected nav button on scroll
+	var $currentPage = $('.page.current-page');
+	$('.navbar li').removeClass('selected');
+	$('.navbar li[data-nav-page='+$currentPage.attr('id')+']').addClass('selected');
+	// Fade background with scroll
+	// var pageVisibilityPercentage = ($('.page.current-page').offset().top - $(window).scrollTop()) / $(window).height();
+	// var currentPageVisibilityPercentage = Math.abs((pageVisibilityPercentage - 0.5) * 2);
+	// var currentPageOffsetPercentage = Math.abs(currentPageVisibilityPercentage - 1);
+	// var opacity = ((currentPageOffsetPercentage * 0.2) + 0.2).toFixed(2);
+	// $('.page').css('background-color', 'rgba(0, 0, 0, '+opacity+')');
 });
 
 // Scroll nav-page to top
@@ -74,14 +93,7 @@ $('#next-page-button').click(function(event) {
 function _scrollToPage($page) {
 	if (!PAGESCROLL_DISABLED) {
 		$('html, body').animate({scrollTop: $page.offset().top}, PAGESCROLL_ANIMATION_SPEED);
-		$('.page').removeClass('current-page');
-		$page.addClass('current-page');
-		if ($page.attr('id') == $('.page').last().attr('id')) {
-			$('#next-page-button').addClass('disabled');
-		}
-		else {
-			$('#next-page-button').removeClass('disabled');
-		}
+		_setCurrentPage($page);
 	}
 	// Disable page scrolling temporarily - prevents infinite scroll loop
 	PAGESCROLL_DISABLED = true;
@@ -92,5 +104,22 @@ function _enablePageScroll() {
 	PAGESCROLL_DISABLED = false;
 }
 
-/* Utility functions */		// TODO: remove before release
+function _setCurrentPage($page) {
+	$('.page').removeClass('current-page');
+	$page.addClass('current-page');
+	if ($page.attr('id') == $('.page').last().attr('id')) {
+		$('#next-page-button').addClass('disabled');
+	}
+	else {
+		$('#next-page-button').removeClass('disabled');
+	}
+}
+
+/* Media query helper functions */
+function _screenSizeIsSMAndUp() { return ($('.media-query-trigger-sm').css('float') == 'none'); }
+function _screenSizeIsMDAndUp() { return ($('.media-query-trigger-md').css('float') == 'none'); }
+function _screenSizeIsLGAndUp() { return ($('.media-query-trigger-lg').css('float') == 'none'); }
+function _screenSizeIsXLAndUp() { return ($('.media-query-trigger-xl').css('float') == 'none'); }
+
+/* Utility functions - for development */
 function log(argument) { console.log(argument); }
